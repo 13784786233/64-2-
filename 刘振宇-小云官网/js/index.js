b5b091166718c64ffe8aca362849d6683f1fcb72
liuzhenyu <2947195327@qq.com>
2020-11-19 15:05:50 +0800

    刘振宇11.19上传

---
+++ 刘振宇-小云官网/js/index.js
@@ -0,0 +1,8 @@
+// function changefs(){
+//     var iw=window.innerWidth;
+//     var ih=window.innerHeight;
+//     var fs=iw*100/1200;
+//     document.getElementsByTagName("html")[0].style.fontSize=fs+"px";
+// }
+// window.onresize=changefs;
+// changefs();
