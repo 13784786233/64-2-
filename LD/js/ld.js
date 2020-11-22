c2689696166825ee7cd9c58ff393d65dc0cb7c69
Liu Dou <1390424476@qq.com>
2020-11-22 16:52:30 +0800

    最新boots

--- LD/js/ld.js
+++ LD/js/ld.js
@@ -2,4 +2,24 @@ $(function(){
     $(".b1").click(function(){
         $(this).parent().next().find(".u1").slideToggle()
     })
+    function aa(){
+        var a=0
+      var b=0
+      function cc(){
+        z=b-a
+      if(z>0){
+          $(".bac").stop().animate({"top":"-91px"},300)
+        }else{
+          $(".bac").stop().animate({"top":"0"},300)
+        }
+     }
+  
+    $(window).scroll(function(){
+     b=$(window).scrollTop()
+        cc()
+        a=b
+     })
+      }
+      aa()
 })
+    
